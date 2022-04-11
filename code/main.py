32ffe81dea42759aa5ff2ed3394eac3c07a79f08
chh <chenghonghong>
2022-04-11 08:34:18 +0800

    conflict_initial

--- code/main.py
+++ code/main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print("first pycharm")
     print("hot-fix version")
+    print("master_conflict")
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
